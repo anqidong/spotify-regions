19a44a6fd7c0424f08529e8908673268d1fb48b8
anqidong <Sprockgafuel+g@gmail.com>
2015-01-29 23:29:33 -0500

    actual initial commit
    add stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spotify-regions
+Shows which regions a Spotify entity is available in. 
---
+++ iso-map-gen.py
@@ -0,0 +1,24 @@
+# data source: http://www.nationsonline.org/oneworld/country_code_list.htm
+
+import json
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+html = urllib.request.urlopen(
+    "http://www.nationsonline.org/oneworld/country_code_list.htm").read()
+
+soup = BeautifulSoup(html)
+
+dict = {}
+
+rows = soup.find(id="codelist")
+for row in rows.find_all("tr", recursive=False):
+    cells = row.find_all("td", recursive=False)
+    if cells: # ignore the header row
+        code =    cells[2].get_text().strip()
+        country = cells[1].get_text().strip()
+        dict[code] = country
+
+file = open("country-codes.json", "w")
+json.dump(dict, file)
